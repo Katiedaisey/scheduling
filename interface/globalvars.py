8368f6dbed9b9807d1f8589d7d7e7c7602d3870f
Katie.Daisey <katiedaisey@gmail.com>
2017-01-23 13:25:00 -0500

    pref params to global
    switches preference and conflict weights to globalvars

--- interface/globalvars.py
+++ interface/globalvars.py
@@ -8,3 +8,18 @@ matrix_sections = '' # actual sec_sec_matrix
 
 parameters = ''
 para_path = ''
+
+# conflict and preference parameters
+sec_time_con = -10000000
+sec_sameclass_pref = 2000
+sec_sameprof_pref = 2000
+
+stu_time_con = -5000
+stu_class_con = -500
+stu_prof_con = -500
+stu_year_pref = 1000
+stu_div_pref = 50000
+stu_skill_pref = 1000
+stu_time_pref = 500
+stu_class_pref = 500
+stu_prof_pref = 500
